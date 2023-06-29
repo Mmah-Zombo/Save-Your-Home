27b0a30422bb70ea78c620e325f0dd7925d3f01e
M'mah Zombo <www.mariamzombo@gmail.com>
2023-06-29 20:19:49 +0000

    added the necessary files for the frontend work

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./views/*.{html,js}", "./scripts/*.js"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'righteous': ['Righteous-Regular', 'sans-serif'],
+      'sand': ['Quicksand', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'green': '#214e34',
+        'charcoal': '#364156',
+        'oxford_blue': '#011638'
+      },
+    },
   },
   plugins: [],
 }
