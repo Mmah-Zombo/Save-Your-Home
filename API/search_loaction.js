c0344f5038be26959e26119c94b6e6ac7cdb6121
M'mah Zombo <www.mariamzombo@gmail.com>
2023-07-04 10:23:49 +0000

    created an env and gitignore file for my tokens

--- API/search_loaction.js
+++ API/search_loaction.js
@@ -8,7 +8,6 @@ const main = document.getElementById("main");
 const display = document.getElementById("options");
 
 let area;
-const API = '2db6e1a8a6684d5b67113fd3227a77b4';
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -21,14 +20,26 @@ form.addEventListener("submit", (event) => {
     main.classList.add("relative");
 
     area = form.place.value;
-    console.log(area);
-
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${area}&limit=5&appid=${API}`)
     .then(res => res.json())
     .then(data => {
         console.log(data);
-        console.log(data[0])
-        // showWeatherData(data);
+        // for (let each of data){
+        //     const divElement = document.createElement('div');
+        //     divElement.className = 'w-full h-1/5 flex items-center border-b border-white';
+
+        //     divElement.innerHTML = 
+        //     `
+        //     <p class="text-4xl pr-8" id="place">${each['name']}</p>
+        //     <p class="text-2xl pr-8" id="state">${each['state']}</p>
+        //     <p class="text-2xl pr-8" id="country">${each['country']}</p>
+        //     `
+        //     display.appendChild(divElement)
+
+        //     each.addEventListener("click", (event) => {
+        //         // 
+        //     })
+        // }
     })
 });
      
@@ -53,9 +64,7 @@ form2.addEventListener("submit", (event) => {
             `
             display.appendChild(divElement)
 
-            each.addEventListener("click", (event) => {
-                // 
-            })
+            console.log('done')
         }
         // showWeatherData(data);
     })
